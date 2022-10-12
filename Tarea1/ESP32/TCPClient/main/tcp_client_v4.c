7d624d9ec5a0ad1ae08591b2fe4ca2691d66a495
valesteban <valentina.esteban@ug.uchile.cl>
2022-10-12 00:12:09 -0300

    inicializar servidor test

--- Tarea1/ESP32/TCPClient/main/tcp_client_v4.c
+++ Tarea1/ESP32/TCPClient/main/tcp_client_v4.c
@@ -19,10 +19,10 @@
 #if defined(CONFIG_EXAMPLE_IPV4)
 #define HOST_IP_ADDR CONFIG_EXAMPLE_IPV4_ADDR
 #elif defined(CONFIG_EXAMPLE_SOCKET_IP_INPUT_STDIN)
-#define HOST_IP_ADDR ""
+#define HOST_IP_ADDR "192.168.28.1"
 #endif
 
-#define PORT CONFIG_EXAMPLE_PORT
+#define PORT 5010
 
 static const char *TAG = "test";
 static const char *payload = " OwO ";
@@ -47,41 +47,36 @@ void tcp_client(void)
         struct sockaddr_storage dest_addr = { 0 };
         ESP_ERROR_CHECK(get_addr_from_stdin(PORT, SOCK_STREAM, &ip_protocol, &addr_family, &dest_addr));
 #endif
-
+        //SE CREO EL SOCKET TCP 
         int sock =  socket(addr_family, SOCK_STREAM, ip_protocol);
         if (sock < 0) {
-            ESP_LOGE(TAG, "Unable to create socket: errno %d", errno);
+            ESP_LOGE(TAG, "No su pudo crear socket: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Socket created, connecting to %s:%d", host_ip, PORT);
+        ESP_LOGI(TAG, "Socket creado, conectando a %s:%d", host_ip, PORT);
 
+        //CONECTA AL SERVIDOR
         int err = connect(sock, (struct sockaddr *)&dest_addr, sizeof(dest_addr));
         if (err != 0) {
-            ESP_LOGE(TAG, "Socket unable to connect: errno %d", errno);
+            ESP_LOGE(TAG, "Socket no se pudo conectar: errno %d", errno);
             break;
         }
-        ESP_LOGI(TAG, "Successfully connected");
+        ESP_LOGI(TAG, "Conecxion exitosa");
 
-        while (1) {
-            int err = send(sock, payload, strlen(payload), 0);
-            if (err < 0) {
-                ESP_LOGE(TAG, "Error occurred during sending: errno %d", errno);
-                break;
-            }
-
-            int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
-            // Error occurred during receiving
-            if (len < 0) {
-                ESP_LOGE(TAG, "recv failed: errno %d", errno);
-                break;
-            }
-            // Data received
-            else {
-                rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
-                ESP_LOGI(TAG, "Received %d bytes from %s:", len, host_ip);
-                ESP_LOGI(TAG, "%s", rx_buffer);
-            }
+        //RECIVE PAQUETE DEL SERVIDOR INDICANDOLE EL TIPO DE CONEXION TCP O UDP 
+        //Y EL PROTOCOLO 0,1,2,3,4
+    
+        int len = recv(sock, rx_buffer, sizeof(rx_buffer) - 1, 0);
+        
+        if (len < 0) {
+            ESP_LOGE(TAG, "recv fallo: errno %d", errno);
+            break;
+        }else {
+            rx_buffer[len] = 0; // Null-terminate whatever we received and treat like a string
+            ESP_LOGI(TAG, "Recibimos %d bytes del host  %s:", len, host_ip);
+            ESP_LOGI(TAG, "%s", rx_buffer);
         }
+        
 
         if (sock != -1) {
             ESP_LOGE(TAG, "Shutting down socket and restarting...");
--- Tarea1/Raspberry/main_server.py
+++ Tarea1/Raspberry/main_server.py
@@ -1,24 +1,82 @@
 import socket
+from xmlrpc.client import TRANSPORT_ERROR
+import db
 
 # "192.168.5.177"  # Standard loopback interface address (localhost)
-HOST = "192.168.28.1"#"localhost"
+HOST = "192.168.28.1" #"localhost"
 PORT = 5010  # Port to listen on (non-privileged ports are > 1023)
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM) #TCP
-s.bind((HOST, PORT))
-s.listen(5)
-print(f"Listening on {HOST}:{PORT}")
+#cosas por mientras reemplazando bbdd
+# 0 = TCP | 1 = UDP
+ID_PROTOCOL =   0         #  0,1,2,3,4 
+TRANSPORT_LAYER =  0       # 0 = TCP | 1 = UDP
 
-conn, addr = s.accept()
-print(f'Conectado por alguien ({addr[0]}) desde el puerto {addr[1]}')
-data = conn.recv(1024)
-if data == b'':
-    print(f"termino no data {data}")
-print(f"Recibido {data}")
-conn.close()
-print('Desconectado')
+#---TCP--CONNECTION ----------------------------------------------------------------------
+
+def iniciar_servidor() -> None:
+    """
+    Inicializa el servidor con una conexion TCP con el ESP32.
+
+    Cuando se logra la conexion, se realiza una consulta a la base de datos
+    preguntando por el protocolo a usar.
+    """
+    
+    print("Hacer conexion TCP")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #TCP
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Listening on {HOST}:{PORT}")
+        # conn is a new socket object usable to send and receive data on the connection.
+        # address is the address bound to the socket on the other end of the connection.
+        conn, addr = s.accept()
+        print(f'Conectado por alguien ({addr[0]}) desde el puerto {addr[1]}')
+
+        # Consulta BD (2) 
+        # TODO
+        data = db.get_protocol()
+        s.sendall(data)
+#-----------------------------------------------------------------------------------------
+
+"""
 
 #AQUI IRA UNA FUNCION QUE REVISA QUE TIPO DE PROTOCOLO OCUPAR Y SI OCUPAR UDP O TCP
+if TRANSPORT_LAYER == 0: #TCP
+
+    Crea una conexion TCP 
+
+    
+    print("Hacer conexion TCP")
+    with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s: #TCP
+        s.bind((HOST, PORT))
+        s.listen()
+        print(f"Listening on {HOST}:{PORT}")
+        # conn is a new socket object usable to send and receive data on the connection.
+        # address is the address bound to the socket on the other end of the connection.
+        conn, addr = s.accept()
+        print(f'Conectado por alguien ({addr[0]}) desde el puerto {addr[1]}')
+
+        
+        data = conn.recv(1024)
+        if data == b'':
+            print(f"Termino no data {data}")
+        print(f"Recibido {data}")
+        
+else:           #UDP
+    print("Hacer conexion UDP")
+
+    with socket.socket(socket.AF_INET, socket.SOCK_DGRAM) as s:
+        s.bind((HOST, PORT))
+        print(f"Listening for UDP packets in {HOST}:{PORT}")
+        
+        data = s.recv(1024)
+        if data == b'':
+            print(f"No llego na-> {data}")
+        print(f"Recibido -> {data}")
+        s.close()
+        print("Desconectado")  
+
+"""
+
 
 
 #INICIAR CONEXION TCP O UDP
@@ -27,3 +85,8 @@ print('Desconectado')
 #IR GUARDANDO PAQUETES EN BBDD
 
 
+if __name__ == "__main__":
+    """
+    Inicializar el servidor
+    """
+    iniciar_servidor()
