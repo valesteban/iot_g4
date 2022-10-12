e95af58d21a955d0c3aa0ce4aa0f44bc2681260e
malva28 <valentina.garrido@ug.uchile.cl>
2022-10-11 23:25:09 -0300

    sensores y protocolos añadidos

---
+++ Tarea1/ESP32/empaquetamiento.c
@@ -0,0 +1,326 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#include "sensores.c"
+
+#define PROTOCOL0_MSG_LEN 6
+#define PROTOCOL1_MSG_LEN 16
+#define PROTOCOL2_MSG_LEN 20
+#define PROTOCOL3_MSG_LEN 44
+#define PROTOCOL4_MSG_LEN 24016
+
+#define ACC_ARRAY_LEN 20
+
+typedef struct {
+    unsigned int id;
+    unsigned long mac;
+    unsigned char tlayer;
+    unsigned char protocol;
+    unsigned int lenmsg;
+} Header;
+
+unsigned long encodeIdMac(Header* pHeader)
+{
+    unsigned long idMask = pHeader->id;
+    unsigned long encoded = pHeader->mac;
+    unsigned long mask = ~(0xffffuL << 48); // para borrar los últimos 2 bytes
+    encoded = (encoded & mask) | (idMask << 48);
+    return encoded;
+}
+
+int decodeIdMac(unsigned long idmac, Header* pEditedHeader)
+{
+    unsigned int id = idmac >> 48;
+    unsigned long mask = ~(0xffffuL << 48); // para borrar los últimos 2 bytes
+    unsigned long mac = idmac & mask;
+
+    pEditedHeader->id = id;
+    pEditedHeader->mac = mac;
+
+    return 0;
+}
+
+unsigned int encodeTPL(Header* pHeader)
+{
+    unsigned int tlayer = pHeader->tlayer;
+    unsigned int protocol = pHeader->protocol;
+    unsigned int mask = 0x0000ffffu;
+    unsigned int encoded = (pHeader->lenmsg)&mask;
+    encoded = encoded | (tlayer << 24) | (protocol << 16);
+    return encoded;
+}
+
+unsigned int decodeTPL(unsigned int tpl, Header* pEditedHeader)
+{
+    unsigned char tlayer = tpl >> 24;
+    unsigned char protocol = tpl >> 16;
+    protocol = protocol & (0x000000ffu);
+    unsigned int lenmsg = tpl & 0x0000ffffu;
+
+    pEditedHeader->tlayer = tlayer;
+    pEditedHeader->protocol = protocol;
+    pEditedHeader->lenmsg = lenmsg;
+    return 0;
+}
+
+int headerInit(Header* pHeader, 
+               unsigned int id,
+               unsigned long mac,
+               unsigned char tlayer,
+               unsigned char protocol,
+               unsigned int lenmsg)
+{
+    pHeader->id = id;
+    pHeader->mac = mac;
+    pHeader->tlayer = tlayer;
+    pHeader->protocol = protocol;
+    pHeader->lenmsg = lenmsg;
+
+    return 0;
+}
+
+int printHeader(Header* pHeader)
+{
+    printf("{id: %u; mac: %lu; t_layer: %u; protocol: %u; len_msg: %u}", pHeader->id, pHeader->mac, pHeader->tlayer, pHeader->protocol, pHeader-> lenmsg);
+}
+
+typedef struct {
+    Header header;
+    BattSensor battery;
+} Protocol0;
+
+int protocol0Init(Protocol0* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer)
+{
+    Header h = {.id = id, .mac = mac, .tlayer = tlayer, .protocol=0, .lenmsg=PROTOCOL0_MSG_LEN };
+    pro->header = h;
+    battSInit(&(pro->battery));
+
+}
+
+int printProtocol0(Protocol0* pro)
+{
+    printf("PROTOCOL 0\n    HEADER=");
+    printHeader(&(pro->header));
+    printf("\n    MSG=[");
+    printBattS(&(pro->battery));
+    printf("]\n");
+    return 0;
+}
+
+typedef struct {
+    Header header;
+    BattSensor battery;
+    ThpcSensor thpc;
+} Protocol1;
+
+int protocol1Init(Protocol1* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer)
+{
+    Header h = {.id = id, .mac = mac, .tlayer = tlayer, .protocol=1, .lenmsg=PROTOCOL1_MSG_LEN };
+    pro->header = h;
+    battSInit(&(pro->battery));
+    thpcSInit(&(pro->thpc));
+}
+
+int printProtocol1(Protocol1* pro)
+{
+    printf("PROTOCOL 1\n    HEADER=");
+    printHeader(&(pro->header));
+    printf("\n    MSG=[");
+    printBattS(&(pro->battery));
+    printf(",\n\t");
+    printThpcS(&(pro->thpc));
+    printf("]\n");
+    return 0;
+}
+
+typedef struct {
+    Header header;
+    BattSensor battery;
+    ThpcSensor thpc;
+    AccelKpi kpi;
+} Protocol23;
+
+int protocol23Init(Protocol23* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer,
+                 unsigned char protocol,
+                 unsigned int lenmsg)
+{
+    Header h = {.id = id, .mac = mac, .tlayer = tlayer, .protocol=protocol, .lenmsg=lenmsg };
+    pro->header = h;
+    battSInit(&(pro->battery));
+    thpcSInit(&(pro->thpc));
+    accelKInit(&(pro->kpi));
+}
+
+int protocol2Init(Protocol23* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer)
+{
+    protocol23Init(pro,
+                   id,
+                   mac,
+                   tlayer,
+                   2,
+                   PROTOCOL2_MSG_LEN);
+}
+
+int protocol3Init(Protocol23* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer)
+{
+    protocol23Init(pro,
+                   id,
+                   mac,
+                   tlayer,
+                   3,
+                   PROTOCOL3_MSG_LEN);
+}
+
+int printProtocol23(Protocol23* pro)
+{
+    printf("PROTOCOL %d\n    HEADER=", (pro->header).protocol);
+    printHeader(&(pro->header));
+    printf("\n    MSG=[");
+    printBattS(&(pro->battery));
+    printf(",\n\t");
+    printThpcS(&(pro->thpc));
+    printf(",\n\t");
+
+    if((pro->header).protocol == 2)
+    {
+        printf("{rms: %.4f}", (pro->kpi).rms);
+    }
+    else
+    {
+        printAccelK(&(pro->kpi));
+    }
+
+    printf("]\n");
+    return 0;
+}
+
+typedef struct {
+    Header header;
+    BattSensor battery;
+    ThpcSensor thpc;
+    AccelSensor acc;
+} Protocol4;
+
+int protocol4Init(Protocol4* pro, 
+                 unsigned int id,
+                 unsigned long mac,
+                 unsigned char tlayer)
+{
+    Header h = {.id = id, .mac = mac, .tlayer = tlayer, .protocol=4, .lenmsg=PROTOCOL4_MSG_LEN };
+    pro->header = h;
+    battSInit(&(pro->battery));
+    thpcSInit(&(pro->thpc));
+    accelInit(&(pro->acc), ACC_ARRAY_LEN);
+}
+
+int protocol4Destroy(Protocol4* pro)
+{
+    accelDestroy(&(pro->acc));
+    return 0;
+}
+
+int printProtocol4(Protocol4* pro)
+{
+    printf("PROTOCOL 4\n    HEADER=");
+    printHeader(&(pro->header));
+    printf("\n    MSG=[");
+    printBattS(&(pro->battery));
+    printf(",\n\t");
+    printThpcS(&(pro->thpc));
+    printf(",\n\t");
+    printAccelP(&(pro->acc));
+    printf("]\n");
+    return 0;
+}
+
+int main()
+{
+    time_t t0 = time(0);
+    Header h = { .id = 8, .mac = 404, .tlayer = 1, .protocol = 4, .lenmsg = 14 };
+    Header h2 = {};
+    printHeader(&h);
+    printf("\n");
+    printHeader(&h2);
+    printf("\nholi\n");
+    unsigned long idmac = encodeIdMac(&h);
+    unsigned int tpl = encodeTPL(&h);
+
+    printf("encoded: %lu\n", idmac);
+    printf("encoded: %u\n", tpl);
+
+    decodeIdMac(idmac, &h2);
+    printHeader(&h2);
+    
+    printf("\n");
+    decodeTPL(tpl, &h2);
+    printHeader(&h2);
+
+    time_t t1 = time(0);
+    printf("\n Time 0: %d, not truncated: %ld \n", t0, t0);
+
+
+    Protocol0 p0;
+    protocol0Init(&p0, 8, 404, 1);
+    printProtocol0(&p0);
+
+    Protocol1 p1;
+    protocol1Init(&p1, 8, 404, 1);
+    printProtocol1(&p1);
+
+    Protocol23 p2;
+    protocol2Init(&p2, 8, 404, 1);
+    printProtocol23(&p2);
+
+    Protocol23 p3;
+    protocol3Init(&p3, 8, 404, 1);
+    printProtocol23(&p3);
+
+    unsigned char test[100];
+    encodeAccelK(&(p3.kpi), test, 0);
+    printf("Encoded accelK: %s\nTEST DECODE: ", test);
+
+    AccelKpi ktest;
+    decodeAccelK(&ktest, test, 0);
+    printAccelK(&ktest);
+
+
+    encodeBattS(&(p3.battery), test, 0);
+    printf("\nEncoded battery: %s\nTEST DECODE: ", test);
+
+    BattSensor btest;
+    int readB = decodeBattS(&btest, test, 0);
+    printf("read Bytes: %d\n", readB);
+    printBattS(&btest);
+
+    encodeThpcS(&(p3.thpc), test, 0);
+    printf("\nEncoded thpc: %s\nTEST DECODE: ", test);
+
+    ThpcSensor ttest;
+    int readT = decodeThpcS(&ttest, test, 0);
+    printf("read Bytes: %d\n", readT);
+    printThpcS(&ttest);
+
+    Protocol4 p4;
+    protocol4Init(&p4, 8, 404, 1);
+    printProtocol4(&p4);
+
+    protocol4Destroy(&p4);
+    
+    return 0;
+}
