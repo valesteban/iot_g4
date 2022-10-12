886c3367b16d313cf0d45959f146f456509b20e4
Ephy <vicente.ardiles@ug.uchile.cl>
2022-10-12 02:15:07 -0300

    client recv test added

--- Tarea1/Raspberry/socketTest.py
+++ Tarea1/Raspberry/socketTest.py
@@ -1,12 +1,15 @@
 import socket
 
+HOST = "192.168.28.1" # as both code is running on same pc
+PORT = 5010 # socket server PORT number
 
-def client_program():
-    host = "192.168.28.1"  # as both code is running on same pc
-    port = 5010  # socket server port number
+def echo_client_test():
+    """
+    Crea una conexion echo entre el cliente y el servidor.
+    """
 
     client_socket = socket.socket()  # instantiate
-    client_socket.connect((host, port))  # connect to the server
+    client_socket.connect((HOST, PORT))  # connect to the server
 
     message = input(" -> ")  # take input
 
@@ -20,6 +23,19 @@ def client_program():
 
     client_socket.close()  # close the connection
 
+def client_recv_test():
+    """
+    Crea una conexion que solo recibe respuesta del servidor
+    """
+
+    client_socket = socket.socket()  # instantiate
+    client_socket.connect((HOST, PORT))  # connect to the server
+
+
+    data = client_socket.recv(1024).decode()  # receive response
+    print('Received from server: ' + data)  # show in terminal
+
+    client_socket.close()  # close the connection
 
 if __name__ == '__main__':
-    client_program()
+    client_recv_test()
