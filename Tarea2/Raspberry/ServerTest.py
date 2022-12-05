1f19eeac60f6e63996c1a6e7de46de34119d67d0
Ephyy <vicente.ardiles@ug.uchile.cl>
2022-12-05 01:26:19 -0300

    db model changes

--- Tarea2/Raspberry/ServerTest.py
+++ Tarea2/Raspberry/ServerTest.py
@@ -7,8 +7,8 @@ import socket
 
 
 def tcp_socket_server():
-    HOST_IP = "127.0.0.1"
-    PORT = 5012
+    HOST_IP = "192.168.28.1"
+    PORT = 5000
     # Iniciamos conexion TCP
     s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
@@ -32,7 +32,7 @@ def cambiarConfiguracion(raspberry):
     print("CONFIGURACION CAMBIADA")
 
 
-def server_test():
+def test_status_20():
     raspberry = Raspberry()
     raspberry.set_HostIp("192.168.28.1")
     raspberry.set_Port(5011)
@@ -43,12 +43,52 @@ def server_test():
     new_thread = Thread(target=cambiarConfiguracion, args=(raspberry,))
     new_thread.start()
 
-    
     raspberry.start_status20()
+
+    return None
+
+
+def test_status_20_2():
+    raspberry = Raspberry()
+    raspberry.set_HostIp("192.168.28.1")
+    raspberry.set_Port(5011)
+    configuracion = (3,23,2,400,16,200,4,420,5011,5011,int(ipaddress.IPv4Address("192.168.28.1")),"ssid","pass")
+    raspberry.setConfiguracion(configuracion)
+    raspberry.set_nueva_configuracion(configuracion)
+
+    HOST_IP = "192.168.28.1"
+    PORT = 5000
+    # Iniciamos conexion TCP
+    s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET,socket.SO_REUSEADDR,1)
+    s.bind((HOST_IP, PORT))
+    s.listen()
+    print(f"Iniciando Socket HOST_IP: {HOST_IP}, PORT: {PORT}")
+    print("Listening.....")
+    conn,addr = s.accept()
+    print(f"Conectado con {addr}")
+
+    conn.sendall(str(raspberry.get_current_configuracion()).encode())        
+
+    s.close()
+
+    return None
+
+
+
+def test_status_21():
+    raspberry = Raspberry()
+    raspberry.set_HostIp("192.168.28.1")
+    raspberry.set_Port(5000)
+    configuracion = (3,23,2,400,16,200,4,420,5011,5011,int(ipaddress.IPv4Address("192.168.28.1")),"ssid","pass")
+    raspberry.setConfiguracion(configuracion)
+    raspberry.set_nueva_configuracion(configuracion)
+
+    raspberry.start_status21()
 
 
 if __name__ == "__main__":
     """
     Inicializar el servidor
     """
-    server_test()
+    test_status_21()
